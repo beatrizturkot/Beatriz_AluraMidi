054925ac9df008f5452c90e3454cf04f9d4d1c3f
beatrizturkot <134975096+beatrizturkot@users.noreply.github.com>
2023-10-09 20:00:43 -0300

    kkkkk

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function tocaSomClap () {   
-    document.querySelector ('#som_tecla_Clap').play();
+function tocaSomPom () {   
+    document.querySelector ('#som_tecla_Pom').play();
 }
 
-document.querySelector('.tecla_Clap').onclick = tocaSomClap;
+document.querySelector('.tecla_Pom').onclick = tocaSomPom;
